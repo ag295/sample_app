20dd794cff25a9c324f61bbc713dc700f3e50b52
ag295 <ale3708@yandex.ru>
2012-06-14 03:36:56 -0500

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 SampleApp::Application.routes.draw do
+  
+  match "/contact", :to => "pages#contact"
+  match "/about", :to => "pages#about"
+  match "/help", :to => "pages#help"
+  match "pages/", :to => "pages#home"
+  
   get "pages/home"
 
   get "pages/contact"
   
   get "pages/about"
+  
+  get "pages/help"  
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,6 +64,8 @@ SampleApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
-
 describe PagesController do
-  
-render_views
+
+  render_views
+
+before(:each) do
+  @base_title = "Ruby on Rails Tutorial Sample App"
+end
 
 describe "GET 'home'" do
 it "should be successful" do
@@ -11,7 +14,8 @@ response.should be_success
 end
 it "should have the right title" do
 get 'home'
-response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+response.should have_selector("title",
+:content => @base_title + " | Home")
 end
 end
 describe "GET 'contact'" do
@@ -21,18 +25,20 @@ response.should be_success
 end
 it "should have the right title" do
 get 'contact'
-response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+response.should have_selector("title",
+:content => @base_title + " | Contact")
 end
 end
 describe "GET 'about'" do
 it "should be successful" do
 get 'about'
-response.should be_success
-end
+response.shouldend
+be_success
 it "should have the right title" do
 get 'about'
-response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+response.should have_selector("title",
+:content => @base_title + " | About")
 end
 end
 end
-
+end
